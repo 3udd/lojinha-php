5c9acf162180e50e1b3e1b92865046189fd69106
Eud <follmannedu@gmail.com>
2026-08-14 20:50:36 -0300

    formulário 100% funcional e insert da marca

---
+++ marca.php
@@ -0,0 +1,28 @@
+<?php
+$host = 'localhost';
+$db   = 'lojinha';
+$user = 'postgres';
+$pass = '123';
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$db;", $user, $pass);
+    
+    $sql = "INSERT INTO public.marcas(nome_marca, pais_origem)
+	        VALUES (:mar_nome, :mar_pais);";
+    $stmt = $pdo->prepare($sql);
+
+    $nome = $_POST[':mar_nome'];
+    $pais = $_POST[':mar_pais'];
+
+    $stmt->execute(
+        [
+            ':mar_nome' => $nome,
+            ':mar_pais' => $pais
+        ]
+    );
+
+    echo "Marca inserida com sucesso! com o ID: ". $pdo->lastInsertID();
+
+} catch (PDOException $e) {
+    echo "Erro: " . $e->getMessage();
+}
--- produto.php
+++ produto.php
@@ -1,36 +1,33 @@
 <?php
-// 1. Configurações do Banco de Dados
 $host = 'localhost';
 $db   = 'lojinha';
 $user = 'postgres';
 $pass = '123';
 
 try {
-    // 2. Conexão com o Banco de Dados
     $pdo = new PDO("pgsql:host=$host;dbname=$db;", $user, $pass);
     
-
-    // 4. Preparar a query SQL (com placeholders ':' para segurança)
     $sql = "INSERT INTO public.produtos(nome_produto, preco, estoque, marca_id)
 	        VALUES (:prod_nome, :prod_preco, :prod_estoque, :prod_marca_id)";
     $stmt = $pdo->prepare($sql);
-
-    //Etapa de barreira de dados
-    //Que os jogos comecem
 
     $nome = $_POST[':prod_nome'];
     $preco = $_POST[':prod_preco'];
     $estoque = $_POST[':prod_estoque'];
     $marca_id = $_POST[':prod_marca_id'];
 
-    //
+
     $preco = str_replace(',', '.', $preco);
 
     if (!is_numeric($preco)){
-        die("Você é um usuário do mal");
+        die("Você é um usuário do mal. Preço deve ser um número");
     }
 
-    // 5. Executar passando os dados reais
+    if (!is_numeric($estoque)){
+        die("Você é um usuário do mal. Estoque deve ser um número");
+    }
+
+
     $stmt->execute(
         [
             ':prod_nome' => $nome,
@@ -40,10 +37,8 @@ try {
         ]
     );
 
-    echo "Dados inseridos com sucesso! com o ID: ". 
-            $pdo->lastInsertID();
+    echo "Produto inserido com sucesso! com o ID: ". $pdo->lastInsertID();
 
 } catch (PDOException $e) {
-    // Caso dê algum erro na conexão ou na query
     echo "Erro: " . $e->getMessage();
 }
